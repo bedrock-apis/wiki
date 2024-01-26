580e8702826ebd5e304543c6d81825a7c0837515
Dingsel <dshluka@gmail.com>
2024-01-26 20:55:15 +0100

    more metadata, type for slug, unified function form

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -2,6 +2,9 @@ import fs from "node:fs";
 import { GetFilesTree } from "@/features/functions"
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { MDXComponents } from "mdx/types";
+import { Metadata } from "next";
+
+type StaticSlugParams = { params: Awaited<ReturnType<typeof generateStaticParams>>[number] }
 
 const components = {
     h1: ({ children }) => <h1 className="underline font-bold text-4xl mb-5">{children}</h1>,
@@ -9,7 +12,7 @@ const components = {
 
 } satisfies MDXComponents
 
-export const generateStaticParams = async () => {
+export async function generateStaticParams() {
     const slugs = [];
     for (const filePath of GetFilesTree("./wiki")) {
         const [base, wiki, ...ss] = filePath.split("/");
@@ -20,7 +23,13 @@ export const generateStaticParams = async () => {
     }
     return slugs;
 }
-export default async function GetMarkdownPageView({ params }: any) {
+export default async function GetMarkdownPageView({ params }: StaticSlugParams) {
     const data = fs.readFileSync("./wiki/" + params.slug.join("/") + ".mdx").toString();
     return <MDXRemote source={data} components={components} />
+}
+
+export async function generateMetadata({ params }: StaticSlugParams): Promise<Metadata> {
+    return {
+        title: `${params.slug.map(x => x.charAt(0).toUpperCase() + x.substring(1)).join("->")} on Bedrock API Wiki`
+    }
 }
