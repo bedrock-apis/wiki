8a6e5016e05efbca40d68d5da53cb4ff8b8afc66
ConMaster <64863757+conmaster2112@users.noreply.github.com>
2024-02-11 18:57:30 +0100

    grouping

--- src/components/aside/aside.tsx
+++ src/components/aside/aside.tsx
@@ -6,11 +6,13 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 
-export default function SideBar(params: { menu: [string, string][] }) {
+export default function SideBar(params: { options: {tags:{[k: string]: {title: string, color?: string}}, menus:{[k: string]: {title:string, link: string}[]}}}) {
     //TODO : Fix Re-Rendering Of Component on Resize With Memo
     //const mobile = isMobile()
 
     const [expanded, setExpanded] = useState(true)
+
+
 
     const container = {
         hidden: { opacity: 1, scale: 0 },
@@ -32,6 +34,27 @@ export default function SideBar(params: { menu: [string, string][] }) {
         }
     } satisfies Variants;
 
+
+
+    const arrayComponents = [];
+    let i = 0;
+    for (const tag of Object.keys(params.options.tags)) {
+        if(!(tag in params.options.menus)) continue;
+        const { title, color } = params.options.tags[tag];
+        arrayComponents.push(
+            <div key={"_" + i++} className="rounded-md shadow-md px-2 my-1 text-xl" style={{"backgroundColor":color??"#ff0000"}}>
+                {title}
+            </div>
+        );
+        for (const {title, link} of params.options.menus[tag]) arrayComponents.push(
+                <motion.div key={"__" + i++} variants={item} className="py-0.5 px-2 mx-1 rounded-md cursor-pointer hover:bg-primary">
+                    <Link href={"/" + link}>
+                        <button className="w-full text-left text-xl">{title}</button>
+                    </Link>
+                </motion.div>
+            )
+    }
+
     return <aside className="shadow-md">
         <button className="fixed top-2 left-2 z-40" onClick={() => setExpanded(!expanded)}>
             <svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 -960 960 960" width="40"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg>
@@ -39,18 +62,9 @@ export default function SideBar(params: { menu: [string, string][] }) {
         <AnimatePresence>
             {expanded && <motion.aside initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="w-60 top-10 h-full min-w-60 float-left">
                 <div className="fixed w-60 top-14 h-full bg-secondary border border-highlight min-w-60 float-left">
-                    <div className="bg-accent p-2 m-2 rounded-md shadow-md">
-                        <h1 className="text-center text-slate-50 font-bold text-lg">Some Kind of Title</h1>
-                    </div>
-                    <motion.div variants={container} initial="hidden" animate="visible" className="flex flex-col px-3">
+                    <motion.div variants={container} initial="hidden" animate="visible" className="flex my-4 flex-col px-3">
                         {
-                            params.menu.map((x, i) => (
-                                <motion.div key={i} variants={item} className="py-0.5 px-2 mx-1 rounded-md hover:bg-primary cursor-pointer">
-                                    <Link href={"/" + x[1]}>
-                                        <button className="w-full text-left text-xl">{x[0]}</button>
-                                    </Link>
-                                </motion.div>
-                            ))
+                            arrayComponents
                         }
                     </motion.div>
                 </div>
