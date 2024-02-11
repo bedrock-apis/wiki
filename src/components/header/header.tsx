e587db404bbd3f0c9c9ad6324ace1b901fdd4521
Dingsel <dshluka@gmail.com>
2024-02-11 15:56:27 +0100

    Fixed mobile layout causing API Types text to shift to the next row

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,11 +3,11 @@ import ButtonLink from "../buttonLink/buttonLink"
 export default function Header() {
     return <header className="w-full">
         <div className="fixed bg-secondary border border-highlight h-14 w-full items-center shadow-xl">
-            <div className="ml-3">
-                <ButtonLink link="/" className="mx-10 my-2.5 text-2xl w-min inline font-bold text-nowrap float-left align-middle">
+            <div className="ml-[3.25rem] h-full">
+                <ButtonLink link="/" className="my-2.5 text-2xl w-min inline font-bold text-nowrap float-left align-middle">
                     Scripting Wiki
                 </ButtonLink>
-                <ButtonLink link="/" className="mx-10 my-3 text-xl w-min inline font-bold text-nowrap float-right align-middle">
+                <ButtonLink link="/" className="mr-3 my-3 text-xl w-min inline font-bold text-nowrap float-right align-middle">
                     API Types
                 </ButtonLink>
             </div>
