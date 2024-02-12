9d6dd770393fb0fa819b986510f3ae7246a7d89c
ConMaster <64863757+conmaster2112@users.noreply.github.com>
2024-02-12 16:27:17 +0100

    clean up

--- bin/app/components/buttonLink/buttonLink.tsx
+++
@@ -1,7 +0,0 @@
-import Link from "next/link";
-
-export default function ButtonLink({ name, link }: { name: string, link: string }) {
-    return <Link href={link}>
-        <button>{name}</button>
-    </Link>
-}
--- bin/app/components/header/header.tsx
+++
@@ -1,7 +0,0 @@
-import style from "./header.module.css"
-
-export default function Header() {
-    return <header className={style.container}>
-        Test
-    </header>
-}
