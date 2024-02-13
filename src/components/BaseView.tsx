eae1d18c124120d1b3f1198f1698b230ac2e73fc
Dingsel <dshluka@gmail.com>
2024-02-13 17:19:06 +0100

    mobile optimasation

--- src/components/BaseView.tsx
+++ src/components/BaseView.tsx
@@ -2,16 +2,16 @@
 import SideBar, { SideBarOptions } from "./aside/aside"
 import Footer from "./footer"
 
-export default function BaseView(data: {children: any[] | any, sideBarOptions: SideBarOptions}) {
-    return <div className="w-full sm:flex">
-        <SideBar options={data.sideBarOptions}/>
-        <div className="w-full overflow-y-auto">
-            <main className="min-h-[calc(100vh-8.5rem)] p-[calc(1%+0.5rem)] w-full mt-[3.5rem] break-all">
-               {data.children}
-            </main>
-            <Footer/>
-        </div>
+export default function BaseView(data: { children: any[] | any, sideBarOptions: SideBarOptions }) {
+  return <div className="w-full sm:flex">
+    <SideBar options={data.sideBarOptions} />
+    <div className="w-full overflow-y-auto">
+      <main className="min-h-[calc(100vh-8.5rem)] p-[calc(1%+0.5rem)] w-full mt-[3.5rem] break-all">
+        {data.children}
+      </main>
+      <Footer />
     </div>
+  </div>
 }
 
 /*
