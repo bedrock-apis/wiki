35d09a21c5c90e74c91c5099aaa5acb1f9724200
Dingsel <dshluka@gmail.com>
2024-02-13 16:49:08 +0100

    mediaquerry for mobile layout

--- src/components/BaseView.tsx
+++ src/components/BaseView.tsx
@@ -1,10 +1,9 @@
 "use client";
 import SideBar, { SideBarOptions } from "./aside/aside"
 import Footer from "./footer"
-import { isMobile } from "@/hooks/useSize";
 
 export default function BaseView(data: {children: any[] | any, sideBarOptions: SideBarOptions}) {
-    return <div className={isMobile()?"w-full":"w-full flex"}>
+    return <div className="w-full sm:flex">
         <SideBar options={data.sideBarOptions}/>
         <div className="w-full overflow-y-auto">
             <main className="min-h-[calc(100vh-8.5rem)] p-[calc(1%+0.5rem)] w-full mt-[3.5rem] break-all">
