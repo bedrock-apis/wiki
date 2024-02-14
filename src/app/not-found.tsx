f6ea3e7a08db149a0eeb605041bfe655cace3823
ConMaster <64863757+conmaster2112@users.noreply.github.com>
2024-02-14 13:29:45 +0100

    not-found external image link example

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,6 +5,7 @@ export default function NotFound(){
             <p style={{fontSize:60}}>404 Not found</p>
             <hr className="m-5"/>
             <p className="text-xl">This is not page what are you looking for.</p>
-        </div>  
+        </div>
+        <img src="https://raw.githubusercontent.com/bedrock-apis/wiki/main/public/next.svg"></img>
     </div>
 }
