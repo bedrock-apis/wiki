b44de64a6340e04abe20292aaeae28e28ff6b48c
ConMaster <64863757+conmaster2112@users.noreply.github.com>
2024-02-18 13:16:01 +0100

    moving

--- raw/index.js
+++ raw/index.js
@@ -9,7 +9,7 @@ const build_folder = process.env.__source_path??"wiki_build";
 for (const file of GetFilesTree("wiki")) {
     const [base, ...path] = file.split("/");
     const f = path[path.length - 1];
+    console.log("Moving:", file);
     fs.mkdirSync([build_folder, ...path.slice(0,path.length - 1)].join("/"), {recursive:true});
-    fs.writeFileSync([build_folder, ...path].join("/"), fs.readFileSync(file));
-    console.log(f);
+    fs.writeFileSync([build_folder, ...path.slice(0, path.length - 1), f + (f.endsWith(".md")?"x":"")].join("/"), fs.readFileSync(file));
 }
